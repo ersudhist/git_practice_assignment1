498018e8a179e7e9432e79688029bb4ac55c326b
unknown <kumarsudhist@gmail.com>
2023-01-16 19:31:44 +0530

    Prime problem is done

---
+++ Prime.js
@@ -0,0 +1,12 @@
+let number = 5;
+let factor =0;
+for(let i=1 ; i<number;i++){
+if(number%i==0){
+factor++;
+}
+}
+if(factor==2){
+console.log("prime number");
+}
+else{
+console.log("not a prime number");
